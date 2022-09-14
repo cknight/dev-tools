eeeefc0dcc8751f4c80b6430e37767c7fe3f7c46
Chris Knight <cknight1234@gmail.com>
2022-09-14 10:45:57 +0100

    fix: correct positioning

--- components/toast.tsx
+++ components/toast.tsx
@@ -12,7 +12,7 @@ const errorStyle = `text-white bg-red-500`;
 export function Toast(props: ToastProperties) {
   const styleType = props.type == 'info' ? infoStyle : (props.type == 'warn' ? warnStyle : errorStyle);
  return (
-  <div id={props.id} class={`absolute top-5 right-5 transition-all duration-500 ${props.fade ? 'opacity-100' : 'opacity-0'} ${props.fade ? "translate-x-0 " : "translate-x-full"}`}>
+  <div id={props.id} class={`fixed top-5 right-5 transition-all duration-500 ${props.fade ? 'opacity-100' : 'opacity-0'} ${props.fade ? "translate-x-0 " : "translate-x-full"}`}>
     <div id="toast-simple" class={`${styleType} p-4 inline-flex rounded-lg divide-x divide-gray-200 shadow`} role="alert">
       <div class="pl-4 text-sm font-bold">{props.message}</div>
     </div>
