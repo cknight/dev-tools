732e55ea97e952fb9bb4fec5ab07009095b7be42
Chris Knight <cknight1234@gmail.com>
2022-09-28 11:01:09 +0100

    fix: allow for '+' and ' encoding characters

--- util/uriEncoding.ts
+++ util/uriEncoding.ts
@@ -2,7 +2,7 @@ import { EncodeDecodeError } from "../types.ts";
 
 export function encodeUriInput(input: string): string | EncodeDecodeError {
   try {
-    return encodeURIComponent(input);
+    return encodeURIComponent(input).replace(/'/g,"%27");
   } catch (err) {
     return {msg: "Failed to encode input", err: err};
   }
@@ -10,7 +10,7 @@ export function encodeUriInput(input: string): string | EncodeDecodeError {
 
 export function decodeUriInput(input: string): string | EncodeDecodeError {
   try {
-    return decodeURIComponent(input);
+    return decodeURIComponent(input.replace(/\+/g,  " "));
   } catch (err) {
     return {msg: "Failed to decode input", err: err};
   }
--- util/uriEncoding_test.ts
+++ util/uriEncoding_test.ts
@@ -10,8 +10,9 @@ test({
     assertEquals("hello", encodeUriInput("hello"));
     assertEquals("hello%20world", encodeUriInput("hello world"));
     assertEquals("%3B%2C%2F%3F%3A%40%26%3D%2B%24", encodeUriInput(";,/?:@&=+$"));
-    assertEquals("-_.!~*'()", encodeUriInput("-_.!~*'()"));
+    assertEquals("-_.!~*%27()", encodeUriInput("-_.!~*'()"));
     assertEquals("%D1%88%D0%B5%D0%BB%D0%BB%D1%8B", encodeUriInput("шеллы"));
+    assertEquals("key%3D%22value%22%26anotherKey%3D%27value%27%26withSpace%3Da%20b%20c", encodeUriInput("key=\"value\"&anotherKey='value'&withSpace=a b c"));
     assertEquals("Failed to encode input", (encodeUriInput("\uD800") as EncodeDecodeError).msg);
   }
 });
@@ -24,6 +25,7 @@ test({
     assertEquals(";,/?:@&=+$", decodeUriInput("%3B%2C%2F%3F%3A%40%26%3D%2B%24"));
     assertEquals("-_.!~*'()", decodeUriInput("-_.!~*'()"));
     assertEquals("шеллы", decodeUriInput("%D1%88%D0%B5%D0%BB%D0%BB%D1%8B"));
+    assertEquals("key=\"value\"&anotherKey='value'&withSpace=a b c", decodeUriInput("key%3D%22value%22%26anotherKey%3D%27value%27%26withSpace%3Da%20b%20c"));
     assertEquals("Failed to decode input", (decodeUriInput("%E0%A4%A") as EncodeDecodeError).msg);
   }
 });
