0895c7b15700055215287841ee61872f0bc54cb1
Chris Knight <cknight1234@gmail.com>
2022-11-09 16:11:16 +0000

    WIP: dark mode (and fresh upgrade)

--- util/styles.ts
+++ util/styles.ts
@@ -8,3 +8,9 @@ export const insertHighlight = `bg-[#acf2bd] pt-0.5`;
 export const deleteColor = `bg-[#fee8e9]`;
 export const deleteHighlight = `bg-[#fdb8c0] pt-0.5`;
 export const emptyRow = `h-6`;
+export const darkMode_background = `bg-[#0f172a]`;
+export const darkMode_primaryText = `text-[#e2e8f0]`;
+export const baseStyles = `
+  html, body {height: 100%;} 
+  path {fill:#d43900} 
+  body.dark path {fill: #da5d2f;}`;
