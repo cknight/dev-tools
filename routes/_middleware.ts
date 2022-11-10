614696847cf051835411900be62377477f19b26e
Chris Knight <cknight1234@gmail.com>
2022-11-10 20:47:53 +0000

    feat: add region to log string

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -10,10 +10,13 @@ export async function handler(
   if (!url.includes("favicon.ico") 
       && !url.includes("diff_match_patch") 
       && !url.includes(".css") 
+      && !url.includes(".php") 
+      && !url.includes("/wp-includes/")
       && !url.includes("_frsh") 
       && !url.includes(".txt")) {
     const referrer = req.headers.get("referer") || 'no-referer';
-    console.log(`${req.method} ${url} ${resp.status} ${Date.now() - start}ms ${referrer}`);
+    const region = Deno.env.get("DENO_REGION") || 'no-region';
+    console.log(`${req.method} ${region} ${url} ${resp.status} ${Date.now() - start}ms ${referrer}`);
   }
 
   return resp;
