f6f165637b2951417452eb6e0474ccd8cb040c24
Chris Knight <cknight1234@gmail.com>
2023-02-19 22:40:02 +0000

    fix: use decode not encode

--- islands/Decode_Url.tsx
+++ islands/Decode_Url.tsx
@@ -6,7 +6,7 @@ export default function UrlDecode() {
   return (
     <div>
       Decode a URL containing <a class={linkStyles} target="_blank" href="https://en.wikipedia.org/wiki/URL_encoding">percent encoded</a> characters into the original string. See also <a class={linkStyles} href="/url-decode">URL Decode</a>.
-      <InputOutput encodingType={"url-encode"}/>
+      <InputOutput encodingType={"url-decode"}/>
     </div>
   );
 }
